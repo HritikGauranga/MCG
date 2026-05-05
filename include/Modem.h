fb447e9d4765a66b72fd814bb4c0bd32bf5a86c0
unknown <hritik.n@gaurangatech.com>
2026-05-05 10:28:30 +0530

    Queue Size increases to 64 13.0

--- include/Modem.h
+++ include/Modem.h
@@ -12,5 +12,5 @@ String sendAT(const String &cmd, int timeout = 3000);
 bool waitForNetwork();
 bool modemSimReady();
 bool sendSMS(const String &number, const String &message);
-bool Modem_init(size_t queueLength = 16);
+bool Modem_init(size_t queueLength = 64);
 void Modem_task(void *pvParameters);
